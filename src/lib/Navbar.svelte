f1a5badfe3e6d28278523f7f0b7998e436297c03
ischo <ischo@LAPTOP-9GOA4UHJ.localdomain>
2023-11-28 14:54:34 +0800

    user can now login, now issues with error message

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,5 +1,13 @@
 <script>
+	import {isLoggedIn} from "../utils/auth";
+	import {logOut} from "../utils/auth";
+	
     let LoggedInValue = false;
+
+	isLoggedIn.subscribe((value)=>{
+		LoggedInValue = value;
+	});
+
 </script>
 
 <div class="navbar bg-base-100 sticky top-0">
@@ -9,7 +17,7 @@
 		<ul class="menu menu-horizontal px-1"><li><a class=""><img class=" btn-circle bg-white w-7 h-7" src="https://www.svgrepo.com/show/309493/dark-theme.svg" alt="Change Theme"></a></li></ul>
 		<!-- show logout if user in not logged in, log in and sign up other wise -->
 		{#if LoggedInValue}
-		<ul class="menu menu-horizontal px-1"><li><a href="/users/login">Log Out</a></li></ul>
+		<ul class="menu menu-horizontal px-1"><li><a on:click={logOut} href="/users/login">Log Out</a></li></ul>
 		{:else}
 		<ul class="menu menu-horizontal px-1"><li><a href="/users/login">Log In</a></li></ul>
 		<ul class="menu menu-horizontal px-1"><li><a href="/users/new">Sign up</a></li></ul>
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -73,7 +73,7 @@ export async function authenticateUser(email, password){
                 'Content-Type':'application/json'
             },
             body: JSON.stringify({
-                identity: email,
+                email,
                 password
             })
         }
